859fe31b5b5ac895b39c59a1135c93f4c42723e0
FilipToth <filip.toth@accelley.com>
2024-09-02 17:00:25 -0400

    Use regular non-np lists to store jobs in the environment

--- src/environments/env.py
+++ src/environments/env.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
+import enum
+import random
 import numpy as np
 import gymnasium as gym
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 
 # https://gymnasium.farama.org/tutorials/gymnasium_basics/environment_creation/
 # https://people.csail.mit.edu/alizadeh/papers/deeprm-hotnets16.pdf
@@ -11,65 +13,104 @@ JOB_QUEUE_SIZE = 4
 NUM_RESOURCES = 2
 RESOURCE_TIME_BUFFER_SIZE = 4
 
+class ResourceType(enum.Enum):
+    CPU = 0
+    MEMORY = 1
+
+
+class Job:
+    empty = False
+
+    def __init__(self, resource_use: dict[ResourceType, float], time: int) -> None:
+        self.resource_use = resource_use
+        self.scheduled = False
+
+    def new_random() -> Job:
+        resources = {}
+        for res in ResourceType:
+            use = random.uniform(0, 1)
+            resources[res] = use
+
+        time = random.randint(1, RESOURCE_TIME_BUFFER_SIZE)
+
+        job = Job(resources, time)
+        return job
+
+    def new_empty() -> Job:
+        resources = {}
+        for res in ResourceType:
+            resources[res] = 0.0
+
+        job = Job(resources, 0)
+        job.empty = True
+
+        return job
+
+
 class MachineEnvironment(gym.Env):
     def __init__(self) -> None:
         # note that jobs also
         # have a timeframe,
         # thus the + 1
-        self._resources = np.zeros(shape=(NUM_RESOURCES, RESOURCE_TIME_BUFFER_SIZE), dtype=np.float32)
-        self._job_queue = np.zeros(shape=(NUM_RESOURCES + 1, JOB_QUEUE_SIZE), dtype=np.float32)
+        self._job_queue: list[Job] = []
+        self._running_jobs = []
+        self._resources = {}
 
         # + 1 since the AI can
         # also choose to do nothing
         self.action_space = gym.spaces.Discrete(JOB_QUEUE_SIZE + 1)
 
-    def step(self, action):
-        # progress the resource matrix one step in time
-        # print(self._resources)
-        resources = self._resources
-        resources = np.delete(resources, (0), axis=1)
-
-        new_res_row = np.zeros(shape=(NUM_RESOURCES, 1), dtype=np.float32)
-        resources = np.append(resources, new_res_row, axis=1)
+    def step(self, action: int):
+        # so far we don't have the concept
+        # of time, this will only happen on
+        # each timestep, resource use will
+        # also be a time matrix not a vector
+        self._step_resource_use()
 
         # process action
         if action == JOB_QUEUE_SIZE:
             # skip
             return self._get_obs(), -0.1, False, False, self._get_info()
         else:
-            job = self._job_queue[:, action:action+1]
-            if np.all(job == 0.0):
+            job = self._job_queue[action]
+            if job.empty:
                 # empty job slot
                 return self._get_obs(), -0.1, False, False, self._get_info()
 
-            time_frame = job[NUM_RESOURCES]
-            job_resources = job[0:NUM_RESOURCES, 0:1]
-            res_with_job = resources[0:NUM_RESOURCES, 0:1] + job_resources
+            job_resources = job.resource_use
+            res_with_job = self._resources.copy()
+            for res, am in job_resources.items():
+                res_with_job[res] += am
 
-            is_not_over = np.all(res_with_job <= 1.0)
+            is_not_over = all(r <= 1.0 for r in res_with_job.values())
             if not is_not_over:
                 # the scheduler allocated too many jobs
                 # and now a resource is overused
                 return self._get_obs(), -0.2, False, False, self._get_info()
 
             # remove job from queue
-            self._job_queue[:, action] = 0
+            self._job_queue[action] = Job.new_empty()
 
             # update machine resources
-            resources[0:NUM_RESOURCES, 0:1] = res_with_job
-            self._resources = resources
+            self._resources = res_with_job
 
-        terminated = False
-        if np.all(self._job_queue == 0.0):
-            terminated = True
+        terminated = all(j.empty for j in self._job_queue)
+        state = self._get_obs()
+        info = self._get_info()
 
-        return self._get_obs(), 1, terminated, False, self._get_info()
+        return state, 1, terminated, False, info
 
     def reset(self, *, seed=None, options=None):
         super().reset(seed=seed)
 
-        self._resources = np.zeros(shape=(NUM_RESOURCES, RESOURCE_TIME_BUFFER_SIZE), dtype=np.float32)
-        self._job_queue = np.random.random(size=(NUM_RESOURCES + 1, JOB_QUEUE_SIZE))
+        self._job_queue = []
+        self._resources = {}
+        self._step_resource_use()
+
+        # generate job queue
+        for _ in range(JOB_QUEUE_SIZE):
+            job = Job.new_random()
+            self._job_queue.append(job)
 
         observation = self._get_obs()
         info = self._get_info()
@@ -85,11 +126,22 @@ class MachineEnvironment(gym.Env):
     def _render_frame(self):
         pass
 
-    def _get_obs(self):
-        resources = self._resources.flatten()
-        job_queue = self._job_queue.flatten()
+    def _step_resource_use(self):
+        self._resources = {}
+        for res in ResourceType:
+            self._resources[res] = 0.0
 
-        state = np.concatenate((resources, job_queue))
+    def _get_obs(self):
+        state = []
+        for res_used in self._resources.values():
+            state.append(res_used)
+
+        for job in self._job_queue:
+            assert not job.scheduled
+            state.extend(job.resource_use.values())
+
+        state = np.array(state, dtype=np.float32)
+
         return state
 
     def _get_info(self):
---
+++ src/test_manual.py
@@ -0,0 +1,13 @@
+import environments.env as env
+
+environment = env.MachineEnvironment()
+initial_state, _ = environment.reset()
+print(f'initial state shape: {initial_state.shape}')
+
+while True:
+    environment.render()
+
+    action = int(input())
+    observation, reward, terminated, truncated, _ = environment.step(action)
+    print(f'observation shape: {observation.shape}')
+    print(f'reward: {reward}')
